d959975b51610722bbdad3da62f417a254ac4e32
Goncalo Freitas <goncalofr.freitas@gmail.com>
2025-10-06 22:38:04 +0200

    Wikinimous copy

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Article.destroy_all
+
+10.times do
+  Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs(number: 3).join("\n\n"),
+  )
+end
+
+puts "Created #{Article.count} articles"
